e04eba56772a932e2009d6a00586f15e01f7e245
Bugongo <bugongobugongo@gmail.com>
2020-07-23 00:56:56 -0700

    Create AnythingJava.java
    ndo naanza

---
+++ AnythingJava.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anything.java;
+
+/**
+ *
+ * @author USER NAME
+ */
+public class AnythingJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ System.out.println("radiance 6 in degrees:"+Math.toDegrees(6));
+ System.out.println("radiance 56 in degrees:"+ Math.toDegrees(56));
+ int side1=23;
+ int side2=12;
+ 
+   System.out.println("The hypothesis is"+Math.hypot(side1,side2));
+   
+    
+        
+
+    }
+
+    
+}
